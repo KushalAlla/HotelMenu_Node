cd82be57c2266f065b7201a2f4c46fb2db80e2bf
KushalAlla <kushal15699@gmail.com>
2024-12-30 16:19:06 -0500

    added DummyUser in db.js

--- db.js
+++ db.js
@@ -1,28 +1,15 @@
 const mariadb = require("mariadb");
 
-// Create a connection pool
+//Create a connection pool
 var pool = mariadb.createPool({
-<<<<<<< HEAD
   host: "127.0.0.1",
   port: 3306,
-  user: "root", // MariaDB username
-  password: "1569", // MariaDB password
+  user: "DummyUser", // MariaDB username
+  password: "Password123", // MariaDB password
   database: "Hotel_Menu", // Database name
   connectionLimit: 5, // Connection pool limit
 });
 
 module.exports = Object.freeze({
   pool: pool,
-=======
-    host:'',   //MariaDB IP
-    port : ,  //MariaDB port
-    user: '',    // MariaDB username
-    password: '', // MariaDB password
-    database: 'Hotel_Menu',  // Database name
-    connectionLimit: 5,      // Connection pool limit
-});
-
-module.exports = Object.freeze({
-    pool: pool
->>>>>>> 7d89615e7d27691696f7e97b29912f1cd8c6a621
 });
