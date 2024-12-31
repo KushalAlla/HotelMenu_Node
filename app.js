0c6feb371101115efa4bb0bbbcfb46d4526ffdfd
KushalAlla <kushal15699@gmail.com>
2024-12-31 17:38:08 -0500

    API changes

--- app.js
+++ app.js
@@ -6,12 +6,23 @@ const port = process.env.PORT || 5000;
 const db = require("./db");
 app.use(cors());
 
+// app.get("/getMenu", (req, res) => {
+//   // Get the 'menuday' header (case-sensitive: should be lowercase)
+//   const reqHead = req.headers["menuday"];
+//   console.log("Received MenuDay header:", reqHead); // Log the value of the header
+//   res.send(reqHead);
+// });
+
 app.get("/getMenu", async (req, res) => {
+  const reqHead = req.headers["menuday"];
+  console.log("header received: ", reqHead);
+  const query = "SELECT * FROM menubyday WHERE menuday = ?";
   try {
-    const result = await db.pool.query("select * from menubyday");
-    res.send(result);
+    const [results] = await db.pool.query(query, [reqHead]);
+    res.json(results);
   } catch (err) {
-    throw err;
+    console.log("Db error", err);
+    res.status(500).send("Error. Request failed");
   }
 });
 
